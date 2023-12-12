ad308c6112f31443750faa3479d7de41772cb74e
baltata <128985806+baltata@users.noreply.github.com>
2023-12-12 14:48:34 +0100

    Update banner.js

--- banner.js
+++ banner.js
@@ -2,16 +2,29 @@
 const banner = document.createElement('a');
 banner.href = 'https://galilee.ac/local/membership/plan.php';
 banner.classList.add('countdown-banner');
+banner.innerHTML = `
+  <p>🎄 Plus que <span id="demo"></span> pour profiter des <span id="discountText">70% de réduction!</span> 🎁</p>
+`;
 
-// Create the content for the banner
-const bannerContent = document.createElement('p');
-bannerContent.innerHTML = '🎄 Plus que <span id="demo"></span> pour profiter des <span id="discountText">70% de réduction!</span> 🎁';
-
-// Append the content to the banner element
-banner.appendChild(bannerContent);
+// Style the banner
+banner.style.position = 'fixed';
+banner.style.top = '0';
+banner.style.left = '0';
+banner.style.width = '100%';
+banner.style.backgroundColor = '#fff'; // Change as needed
+banner.style.zIndex = '1000'; // Adjust as needed
+banner.style.textAlign = 'center';
+banner.style.padding = '10px';
+banner.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.1)'; // Optional: Add shadow
 
 // Find the body element and add the banner to the top
 const body = document.querySelector('body');
 if (body) {
   body.insertBefore(banner, body.firstChild);
+}
+
+// Adjust the z-index of the menu bar or conflicting elements
+const menuBar = document.querySelector('.navbar'); // Replace with your menu class
+if (menuBar) {
+  menuBar.style.zIndex = '999'; // Set a z-index lower than the banner
 }
